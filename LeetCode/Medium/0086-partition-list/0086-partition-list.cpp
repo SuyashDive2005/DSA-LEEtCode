da3c6970776ed03c7f98405889cb0d90ed13e747
Suyash Dive <dsapractice2425@gmail.com>
2026-01-15 23:12:02 +0530

    Time: 0 ms (100%), Space: 15.1 MB (9.66%) - LeetHub

---
+++ LeetCode/Medium/0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* small=new ListNode(0);
+        ListNode* large=new ListNode(0);
+        ListNode* s_ptr=small;
+        ListNode* l_ptr=large;
+
+        while(head!=nullptr){
+            if(head->val<x){
+                s_ptr->next=new ListNode(head->val);
+                s_ptr=s_ptr->next;
+            }
+            else{
+                l_ptr->next=new ListNode(head->val);
+                l_ptr=l_ptr->next;
+            }
+            head=head->next;
+        }
+        l_ptr->next=nullptr;
+        s_ptr->next=large->next;
+
+
+        return small->next;
+    }
+};
