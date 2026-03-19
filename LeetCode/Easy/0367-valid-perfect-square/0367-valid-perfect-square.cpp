2fde48820d53edce473c1b8042ccbb3ac2215641
Suyash Dive <dsapractice2425@gmail.com>
2026-03-19 16:00:48 +0530

    Time: 0 ms (100%), Space: 7.6 MB (87.18%) - LeetHub

---
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int root=sqrt(num);
+        if(root*root == num){
+            return true;
+        }else return false;
+    }
+};
