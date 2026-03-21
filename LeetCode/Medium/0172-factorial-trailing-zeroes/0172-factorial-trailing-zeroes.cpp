d27d6689bd2e13789c53a65506eb65e1e3352cfc
Suyash Dive <dsapractice2425@gmail.com>
2026-03-21 16:06:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ LeetCode/Medium/0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int cnt=0;
-        while(n>0){
-            n/=5;
-            cnt+=n;
-        }
-        return cnt;
+        if(n==0 || n==1) return 0;
+        return n/5 + trailingZeroes(n/5);
     }
 };
