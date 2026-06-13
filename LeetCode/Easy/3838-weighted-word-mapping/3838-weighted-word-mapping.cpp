72106145d8b7351cad86c812869b773489300144
Suyash Dive <dsapractice2425@gmail.com>
2026-06-13 20:19:28 +0530

    Time: 0 ms (100%), Space: 43.7 MB (24.57%) - LeetHub

---
+++ LeetCode/Easy/3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans="";
+
+        for(int i=0;i<words.size();i++){
+            int x=0;
+            for(auto &c:words[i]){
+                x+=weights[(c-'a')];
+            }
+
+            if(x>25){
+                x%=26;
+            }
+            ans+=char('z'-x);
+        }
+
+        return ans;
+
+    }
+};
