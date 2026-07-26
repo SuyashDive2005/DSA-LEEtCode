b617554e558faacb0ac28e3c5e5b8f4fcad9a16c
Suyash Dive <dsapractice2425@gmail.com>
2026-07-26 12:17:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (5.31%) - LeetHub

---
+++ LeetCode/Easy/4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestInteger(int n, int s) {
+        string ans="";
+
+        if(s>9*n || s<0) return -1;
+        for(int i=0;i<n;i++){
+            int digit=min(9,s);
+            ans+=to_string(digit);
+            s-=digit;
+        }
+
+        return stoi(ans);
+    }
+};
