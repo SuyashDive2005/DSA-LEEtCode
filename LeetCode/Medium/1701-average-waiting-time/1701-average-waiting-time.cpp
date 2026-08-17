2c08301e2b5bdd5242cc11957a0e454eae29375e
Suyash Dive <dsapractice2425@gmail.com>
2026-08-17 14:40:41 +0530

    Time: 0 ms (100%), Space: 76.9 MB (93.75%) - LeetHub

--- LeetCode/Medium/1701-average-waiting-time/1701-average-waiting-time.cpp
+++ LeetCode/Medium/1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        int n=customers.size();
+        int n = customers.size();
 
-        long long curr=0;
-        long long totaltime=0;
-        for (auto &it : customers) {
-            long long arival=it[0];
-            long long timetaken=it[1];
-            curr=max(arival,curr)+timetaken;
-
-            totaltime+=(curr-arival);
-        
+        long long curr = 0;
+        long long totaltime = 0;
+        for (auto& it : customers) {
+            long long arival = it[0];
+            long long timetaken = it[1];
+            curr = max(arival, curr) + timetaken;
+            totaltime += (curr - arival);
         }
 
-        return (double)totaltime/n;
+        return (double)totaltime / n;
     }
 };
